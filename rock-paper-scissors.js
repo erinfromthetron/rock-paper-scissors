337b3c6870189eaa134283caa3d294c9d3c78757
erinfromthetron <rtomabc789@gmail.com>
2023-05-14 01:21:49 +0700

    rock-paper-scissors.html

---
+++ rock-paper-scissors.js
@@ -0,0 +1,7 @@
+
+let playerScore = 0
+let compScore = 0
+function getComputerChoice() {
+  const rpsList = ["rocks", "paper", "scissors"]
+  var item = rpsList[Math.floor(Math.random()*rpsList.length)]
+}
